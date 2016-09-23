9a0783dd4fe50d895b75deca96ddd394d87d96e4
aacohen <arielaades@gmail.com>
2016-09-23 11:20:10 -0400

    Swift Tuples Lab Complete

--- Tuples.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ Tuples.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,9 +14,7 @@
 /*: question1
  ### 1. Create a tuple to represent a person! The tuple should have a person's name in the first field and their age in the second field. Define the tuple as a variable named `person`.
  */
-// write your code here
-
-
+var person = (name: "Ariela", age: 33)
 
 
 
@@ -26,10 +24,7 @@
 /*: question2
  ### 2. Print out "<Name> is <Age> years old" using the tuple you created in Question 1.
  */
-// write your code here
-
-
-
+print("\(person.name) is \(person.age) years old")
 
 
 
@@ -38,23 +33,14 @@
 /*: question3
  ### 3. It is the person's birthday! Increment the person's age by one year that you created in Question 1.
  */
-// write your code here
-
-
-
-
-
+person.age = (person.age+1)
 
 
 
 /*: question4
  ### 4. Print out "Happy <Age> birthday, <Name>!" using the `person` variable.
  */
-// write your code here
-
-
-
-
+print ("Happy \(person.age)th birthday, \(person.name)!")
 
 
 
@@ -62,11 +48,7 @@
 /*: question5
  ### 5. Create another person variable called `someone` using _named_ fields for the name and age.
  */
-// write your code here
-
-
-
-
+var someone = (name: "Gabrielle", age: 2)
 
 
 
@@ -74,33 +56,20 @@
 /*: question6
  ### 6. Print out "<Name> is <Age> years old" for the `someone` variable you created in Question 5.
  */
-// write your code here
-
-
-
-
+print("\(someone.name) is \(someone.age) years old")
 
 
 
 /*:
  ### 7. It is `someone`'s birthday! Increment `someone`'s age by one year.
  */
-// write your code here
-
-
-
-
-
+someone.age = (someone.age+1)
 
 
 /*:
  ### 8. Print out "Happy <Age> birthday, <Name>!" using the `someone` variable.
  */
-// write your code here
-
-
-
-
+print("Happy \(someone.age)rd birthday, \(someone.name)!")
 
 
 
@@ -108,7 +77,7 @@
 /*:
  ### 9. Create a _constant_ tuple called `human`, with named fields for `name` and `age`.
  */
-// write your code here
+let human = (name: "Ariela", age: 3)
 
 
 
@@ -121,7 +90,7 @@
  ### 10. Can you change `human`'s age? Why or why not?
  */
 
-
+// no because it's a constant. You would need to change the tuple to a var.
 
 
 
